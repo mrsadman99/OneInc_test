6d6d94c0a8cbacaec144e37a32e68209d9490f25
Anton Brechka <brechka.a.wm@gmail.com>
2020-09-11 04:56:17 +0300

    final fix

--- OneInc_test/App_Data/dbo.PolicyType.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[PolicyType] (
-    [Id]   INT           NOT NULL,
-    [Name] NVARCHAR (20) NOT NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
-);
-
--- OneInc_test/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="OneInc_test.MvcApplication" Language="C#" %>
